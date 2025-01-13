52769b115a0644c65bff0c8c7d050f4a51f1bffc
bone1nis <bone1nis@gmail.com>
2025-01-13 07:55:45 +0300

    fix router links

--- assets/AgeVerification-BcCo-6ki.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-BI8DAKkG.js";import{A as t}from"./AgeVerification-CsJpK_k9.js";const o=()=>r.jsx(t,{});export{o as default};
---
+++ assets/AgeVerification-svHHx0TO.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-CQPJHOSU.js";import{A as t}from"./AgeVerification-Cn1Avogz.js";const o=()=>r.jsx(t,{});export{o as default};
