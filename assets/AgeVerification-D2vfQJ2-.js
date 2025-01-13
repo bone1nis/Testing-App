6cf8d5d63e90ca59b873e28684eb283ee7efa210
bone1nis <bone1nis@gmail.com>
2025-01-13 08:06:55 +0300

    fix images links

---
+++ assets/AgeVerification-D2vfQJ2-.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-SxXwSHyv.js";import{A as t}from"./AgeVerification-C9RFw_6l.js";const o=()=>r.jsx(t,{});export{o as default};
--- assets/AgeVerification-svHHx0TO.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-CQPJHOSU.js";import{A as t}from"./AgeVerification-Cn1Avogz.js";const o=()=>r.jsx(t,{});export{o as default};
