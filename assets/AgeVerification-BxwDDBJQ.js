a1a18313c1276050e785e5fe8bdf34430679461c
bone1nis <bone1nis@gmail.com>
2025-01-13 08:27:08 +0300

    fix scss links build

---
+++ assets/AgeVerification-BxwDDBJQ.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-rQ0cTazM.js";import{A as t}from"./AgeVerification-CAn8ypKq.js";const o=()=>r.jsx(t,{});export{o as default};
--- assets/AgeVerification-CQPqmxAk.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-CRj9wbud.js";import{A as t}from"./AgeVerification-CIEYDKNk.js";const o=()=>r.jsx(t,{});export{o as default};
